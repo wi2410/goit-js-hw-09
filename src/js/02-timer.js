f0602f7b93b2929800b6ab85dc9ceb87e90939f6
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-28 18:20:43 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,25 +2,60 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
+
+let choosenDate;
+let intervalId = 0;
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+}
+
+refs.startBtn.setAttribute('disabled', true);
+
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < options.defaultDate) {
+      Notiflix.Notify.failure('Будь ласка, виберіть дату в майбутньому');
+      refs.startBtn.setAttribute('disabled', true);
+      return;
+    }
+    refs.startBtn.removeAttribute('disabled');
+    choosenDate = selectedDates[0].getTime();
   },
 };
 
 flatpickr("input#datetime-picker", options);
 
-const startBtn = document.querySelector('button[data-start]');
+refs.startBtn.addEventListener('click', onClickStartBtn)
 
+function onClickStartBtn() {
+  refs.startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    const deltaTime = convertMs(choosenDate - Date.now());
+    updateClockFace(deltaTime);
+    
+      if (choosenDate - Date.now() < 1000) {
+        clearInterval(intervalId);
+      };
+  }, 1000);
 
+}
 
-
-
-
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
 
 function pad(value) {
   return String(value).padStart(2, `0`);
